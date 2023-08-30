220bfa625fd6076f086cb4ca8d126a76f6d56401
Aladin Júnior <aladinjr013@icloud.com>
2023-08-30 13:39:34 -0300

    text input layout used

--- app/src/main/java/co/aladinjunior/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/co/aladinjunior/instagram/login/view/LoginActivity.kt
@@ -3,10 +3,13 @@ package co.aladinjunior.instagram.login.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.aladinjunior.instagram.R
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewById<TextInputLayout>(R.id.login_input_email).error = "E-mail invalido"
     }
+
 }
