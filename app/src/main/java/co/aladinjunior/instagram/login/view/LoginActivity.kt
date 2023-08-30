34b3a301cf2ebae2df6500fc4c469e7ab080cd33
Aladin Júnior <aladinjr013@icloud.com>
2023-08-29 22:08:14 -0300

    login layout created

--- app/src/main/java/co/aladinjunior/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/co/aladinjunior/instagram/login/view/LoginActivity.kt
@@ -1,11 +1,12 @@
-package co.aladinjunior.instagram
+package co.aladinjunior.instagram.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.aladinjunior.instagram.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
