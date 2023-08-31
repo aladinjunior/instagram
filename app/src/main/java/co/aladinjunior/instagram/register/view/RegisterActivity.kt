e273abcc369c95475d231644f45b6b3db5736f88
Aladin Júnior <aladinjr013@icloud.com>
2023-08-31 14:24:04 -0300

    fragment register account created

---
+++ app/src/main/java/co/aladinjunior/instagram/register/view/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package co.aladinjunior.instagram.register.view
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import co.aladinjunior.instagram.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
