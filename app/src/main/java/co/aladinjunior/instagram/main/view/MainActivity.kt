57448e5fcce8edc324488ebc068287409afe07b5
Aladin Júnior <aladinjr013@icloud.com>
2023-09-01 14:09:06 -0300

    bottom navigation menu created

---
+++ app/src/main/java/co/aladinjunior/instagram/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.aladinjunior.instagram.main.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.aladinjunior.instagram.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
