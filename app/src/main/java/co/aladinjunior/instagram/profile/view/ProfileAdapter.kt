a8e19a0d59016791565c7c949f87aabff76b73ba
Aladin Júnior <aladinjr013@icloud.com>
2023-09-12 13:48:36 -0300

    profile adapter and recyclerview implemented

---
+++ app/src/main/java/co/aladinjunior/instagram/profile/view/ProfileAdapter.kt
@@ -0,0 +1,33 @@
+package co.aladinjunior.instagram.profile.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import co.aladinjunior.instagram.R
+
+class ProfileAdapter : RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
+        return ProfileViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_profile_photos, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
+        holder.bind(R.drawable.ic_insta_add)
+    }
+
+    override fun getItemCount(): Int {
+        return 30
+    }
+
+    inner class ProfileViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        fun bind(image: Int){
+            itemView.findViewById<ImageView>(R.id.profile_img_photos)
+                .setImageResource(image)
+        }
+    }
+
+}
--- app/src/main/java/co/aladinjunior/instagram/profile/view/ProfileFragment.kt
+++ app/src/main/java/co/aladinjunior/instagram/profile/view/ProfileFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.aladinjunior.instagram.R
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val rv = view.findViewById<RecyclerView>(R.id.profile_rv)
+        rv.adapter = ProfileAdapter()
+        rv.layoutManager = GridLayoutManager(requireContext(), 3)
     }
 }
