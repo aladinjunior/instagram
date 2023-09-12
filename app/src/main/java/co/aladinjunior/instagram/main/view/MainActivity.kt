be06b801276bdf4adfaec34fb03433861459a30e
Aladin Júnior <aladinjr013@icloud.com>
2023-09-12 15:06:03 -0300

    customized toolbar implemented

--- app/src/main/java/co/aladinjunior/instagram/main/view/MainActivity.kt
+++ app/src/main/java/co/aladinjunior/instagram/main/view/MainActivity.kt
@@ -2,11 +2,19 @@ package co.aladinjunior.instagram.main.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import co.aladinjunior.instagram.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.title = ""
+
     }
 }
--- app/src/main/java/co/aladinjunior/instagram/profile/view/ProfileFragment.kt
+++ app/src/main/java/co/aladinjunior/instagram/profile/view/ProfileFragment.kt
@@ -1,15 +1,14 @@
 package co.aladinjunior.instagram.profile.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.aladinjunior.instagram.R
 
 class ProfileFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
@@ -19,5 +18,15 @@ class ProfileFragment : Fragment() {
         val rv = view.findViewById<RecyclerView>(R.id.profile_rv)
         rv.adapter = ProfileAdapter()
         rv.layoutManager = GridLayoutManager(requireContext(), 3)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_profile_toolbar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
